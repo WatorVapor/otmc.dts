61a62972e857e6e51249a8a746ee77a6a562a9c0
wator vapor <watorvapor@gmail.com>
2025-10-19 13:53:51 +0000

    provision nginx

---
+++ edgeTwin/provision/genKeys/userKeys.mjs
@@ -0,0 +1,31 @@
+import {createOrLoadKeys,createOrLoadCSR} from '../keyUtils/CertAndKey.mjs';
+import { join } from 'path';
+
+const secureDir = '/secure/enduser/'
+const privClientKeyFilePath = join(secureDir, 'keys', 'client.key.pem');
+const pubClientKeyFilePath = join(secureDir, 'keys', 'client.key_pub.pem');
+
+// C: 国家代码 (Country)
+// ST: 州或省 (State)
+// L: 城市 (Locality)
+// O: 组织 (Organization)
+// OU: 组织单位 (Organizational Unit)
+// CN: 通用名称 (Common Name)
+const clientSubject = {
+  C: 'UN',
+  ST: 'wator',
+  L: 'otmc',
+  O: 'otmc',
+  OU: 'dts',
+  CN: 'Digital Twin Client Certificate for End User Connection'
+};
+const validityYearsClient = 20;
+
+// Certificate Signing Request
+const clientCSRFilePath = join(secureDir, 'ssl', 'client.csr');
+
+const clientKeyPair = await createOrLoadKeys(privClientKeyFilePath, pubClientKeyFilePath);
+console.log('::clientKeyPair:=<', clientKeyPair,'>');
+
+const clientCSR = await createOrLoadCSR(clientCSRFilePath, clientSubject, validityYearsClient,clientKeyPair);
+console.log('::clientCSR:=<', clientCSR,'>');

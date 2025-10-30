462d564b6bd727bb8cff291981641acf346889a7
wator vapor <watorvapor@gmail.com>
2025-10-30 22:38:37 +0000

    provision api

--- edgeTwin/provision/test/provision.add.client.csr.bash
+++ edgeTwin/provision/test/provision.add.client.csr.bash
@@ -22,3 +22,10 @@ curl -X GET \
     --key ssl/client.key.pem \
     --cacert /opt/secure/dts/provision/factory/ssl/rootca.crt \
     https://127.0.0.1:9443/api/v1/health | jq .
+
+# 测试无证书请求
+curl -X POST \
+    -H "Content-Type: text/plain" \
+    --data-binary @ssl/client.csr \
+    --cacert /opt/secure/dts/provision/factory/ssl/rootca.crt \
+    https://127.0.0.1:9443/api/v1/provision/add/client/cert

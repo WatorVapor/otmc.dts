3d5ec719f17cbdff0b42cd8644243eaf83228af5
wator vapor <watorvapor@gmail.com>
2025-11-15 14:43:49 +0000

    minio

--- edgeTwin/minio-cluster/scripts/init.bash
+++ edgeTwin/minio-cluster/scripts/init.bash
@@ -3,4 +3,7 @@ docker exec -it dts-minio /bin/bash -c "mc alias set dts-minio http://dts-minio:
 docker exec -it dts-minio /bin/bash -c "mc admin user add dts-minio dts-builtin tZpNRpGFCQygmbfe"
 docker exec -it dts-minio /bin/bash -c "mc admin policy attach dts-minio readwrite --user=dts-builtin"
 docker exec -it dts-minio /bin/bash -c "mc admin user info dts-minio dts-builtin"
+docker exec -it dts-minio /bin/bash -c "mc admin accesskey create dts-minio dts-builtin" > dts-builtin.access.txt
+
+
 

147a8fdaf4f83678c6c67a85a2ae4ee13c5622a3
wator vapor <watorvapor@gmail.com>
2025-11-16 14:30:51 +0000

    minio

--- edgeTwin/minio-cluster/scripts/create-builtin-bucket.bash
+++ edgeTwin/minio-cluster/scripts/create-builtin-bucket.bash
@@ -2,10 +2,3 @@
 docker exec -it dts-minio /bin/bash -c "mc alias set dts-builtin http://dts-minio:9000 7NMF6I2FWHAA22AUAEC9 72ULbn40iz+yEFqXkQ3mHuW6LoP9CsxwZv+UzyJW"
 docker exec -it dts-minio /bin/bash -c "mc mb dts-builtin/dts-system-setting"
 docker exec -it dts-minio /bin/bash -c "mc mb dts-builtin/dts-docker-repository"
-
-
-# docker exec -it dts-minio /bin/bash -c "mc rb dts-builtin/my-bucket"
-# docker exec -it dts-minio /bin/bash -c "mc rb dts-builtin/dts-system-bucket"
-# docker exec -it dts-minio /bin/bash -c "mc rb dts-builtin/dts-system"
-
-

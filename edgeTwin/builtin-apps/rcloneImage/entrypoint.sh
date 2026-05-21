f9fab3dc805cd81c4dfbbcc05c8de6380776fc56
Wator Vapor <watovapor@gmail.com>
2026-05-21 06:32:23 +0000

    good spiffe

--- edgeTwin/builtin-apps/rcloneImage/entrypoint.sh
+++ edgeTwin/builtin-apps/rcloneImage/entrypoint.sh
@@ -2,21 +2,13 @@
 
 cleanup() {
     echo "Received termination signal. Shutting down..."
-    kill -TERM "$AGENT_PID" 2>/dev/null
-    kill -TERM "$HELPER_PID" 2>/dev/null
-    wait "$AGENT_PID"
-    wait "$HELPER_PID"
+    kill -TERM "$RCLONE_PID" 2>/dev/null
+    wait "$RCLONE_PID"
     exit 0
 }
 trap cleanup SIGTERM SIGINT
 
 /usr/local/bin/rclone &
 RCLONE_PID=$!
-
-echo "Waiting for SPIRE socket..."
-for i in {1..30}; do
-    [ -S "$SPIRE_SOCKET_PATH" ] && break
-    sleep 1
-done
 
 wait "$RCLONE_PID"
